600a41ff64ecc669ad569bd57b3cd6ec49a4d5a9
Manoel Ribeiro Almeida <manoel.ribeiro@unilab.edu.br>
2023-09-25 15:40:18 -0300

    lora fix

--- examples/Receptor/Receptor.ino
+++ examples/Receptor/Receptor.ino
@@ -1,45 +1,52 @@
-#include <SoftwareSerial.h>                               //Biblioteca de comunicação do módulo
-#include <EBYTE.h>                                        //Biblioteca do Kris
-  
+#include <SoftwareSerial.h>  //Biblioteca de comunicação do módulo
+#include <EBYTE.h>           //Biblioteca do Kris
+
 #define M0_LoRa 12
 #define M1_LoRa 11
 #define RX_LoRa 9   // Vai no TXD do módulo
 #define TX_LoRa 10  // Vai no RXD do módulo
 #define AUX_LoRa 8
-                            
-SoftwareSerial lora(RX_LoRa, TX_LoRa);                   //Definição dos pinos para criar a comunicação serial
-EBYTE LoRa(&lora, M0_LoRa, M1_LoRa, AUX_LoRa);           //Criar objeto de Transmissão, passando na comunicação serial e pinos
+
+
+SoftwareSerial lora(RX_LoRa, TX_LoRa);          //Definição dos pinos para criar a comunicação serial
+EBYTE LoRa(&lora, M0_LoRa, M1_LoRa, AUX_LoRa);  //Criar objeto de Transmissão, passando na comunicação serial e pinos
 
 void setup() {
-  
-Serial.begin (9600);                                    //Definir taxa de trabalho em 9600
-lora.begin(9600);
-  LoRa.init();                                         // Inicializa a comunicação e obtem todos os parâmetros do módulo
+
+  Serial.begin(9600);  //Definir taxa de trabalho em 9600
+  lora.begin(9600);
+  LoRa.init();  // Inicializa a comunicação e obtem todos os parâmetros do módulo
 
   //LoRa.Reset();                                       // Reseta parâmetros para os de fábrica
-  LoRa.SetAirDataRate(ADR_2400);                          // Estabelece a taxa de dados de transmissão
-  LoRa.SetAddress(0);                                   // Estabelece o endereço da rede
-  LoRa.SetChannel(16);                                  // Estabelece canal como 23
-  LoRa.SaveParameters(TEMPORARY);                       // Salva todas as definições de forma temporária
+  //LoRa.SetAirDataRate(ADR_2400);   // Estabelece a taxa de dados de transmissão
+  LoRa.SetAddress(1);  // Estabelece o endereço da rede
+  //LoRa.SetChannel(16);             // Estabelece canal como 23
+  LoRa.SaveParameters(TEMPORARY);  // Salva todas as definições de forma temporária
 
-  LoRa.PrintParameters();                               // Imprime todos os parâmetros (configurações) obtidos do módulo 
-  LoRa.SetMode(MODE_NORMAL);                            // Define modo de operação como normal
+  LoRa.PrintParameters();     // Imprime todos os parâmetros (configurações) obtidos do módulo
+  LoRa.SetMode(MODE_NORMAL);  // Define modo de operação como normal
 }
- 
+
+bool receiveTarget(EBYTE LoRa) {
+  if (!LoRa.available()) return false;
+  uint8_t aH = LoRa.GetByte();
+  uint8_t aL = LoRa.GetByte();
+  LoRa.GetByte();
+  if (LoRa.GetAddress() != ((aH << 8) | (aL))) {
+    while (LoRa.available()) {
+      LoRa.GetByte();
+    }
+    return false;
+  };
+  return true;
+}
+
 
 
 void loop() {
-  
-if (lora.available()>0){
-  String input = lora.readString();
-  Serial.print(" A Mensagem do Lora é: "); 
-  for (int i=0;i<input.length();i++) {
-    Serial.print(input[i], HEX);
-    Serial.print(" ");
+  if (receiveTarget(LoRa)) {
+    String input = lora.readString();
+    Serial.print(input);
   }
-  Serial.println();
-  
-  
-}
-delay(1500);
+  delay(1500);
 }
--- examples/Transmissor/Transmissor.ino
+++ examples/Transmissor/Transmissor.ino
@@ -8,6 +8,8 @@
 #define TX_LoRa 10  // Vai no RXD do módulo
 #define AUX_LoRa 8
 
+#define TXM_FIXED 0b1
+
 SoftwareSerial lora(RX_LoRa, TX_LoRa);          //Definição dos pinos para criar a comunicação serial
 EBYTE LoRa(&lora, M0_LoRa, M1_LoRa, AUX_LoRa);  //Criar objeto de Transmissão, passando na comunicação serial e pinos
 
@@ -20,23 +22,44 @@ void setup() {
   //LoRa.Reset();                                      // Reseta parâmetros para os de fábrica
   LoRa.SetAirDataRate(ADR_2400);   // Estabelece a taxa de dados de transmissão
   LoRa.SetAddress(0);              // Estabelece endereço da rede
-  LoRa.SetChannel(16);             // Estabelece canal como 23
+                                   // Estabelece canal como 23
   LoRa.SaveParameters(TEMPORARY);  // Salva todas as definições de forma temporária
 
   LoRa.PrintParameters();     // Imprime todos os parâmetros (configurações) obtidos do módulo
   LoRa.SetMode(MODE_NORMAL);  // Define modo de operação como normal
+  LoRa.SetTransmissionMode(TXM_FIXED);
 }
 
 
+bool getBusy() {
+  if (digitalRead(AUX_LoRa) == HIGH) {
+    return false;
+  } else return true;
+}
 
-
+bool sendTarget(EBYTE LoRa, uint16_t targetAddres, uint8_t targetChannel) {
+  if (LoRa.GetTransmissionMode() != TXM_FIXED) return false;
+  if (!getBusy()) {
+    uint8_t aH = ((targetAddres & 0xFFFF) >> 8);
+    uint8_t aL = (targetAddres & 0xFF);
+    LoRa.SetChannel(targetChannel);
+    LoRa.SendByte(aH);
+    LoRa.SendByte(aL);
+    LoRa.SendByte(targetChannel);
+    return true;
+  } else {
+    return false;
+  }
+}
 
 void loop() {
-
   if (Serial.available() > 0) {
     String msg = Serial.readString();
-    String buffer = (String)char(0) + (String)char(1) + (String)char(16) + msg;
-    lora.print(buffer);
-    Serial.println(buffer);
+    if (sendTarget(LoRa, 2, 16)) {
+      for (int i = 0; i < msg.length(); i++) {
+        LoRa.SendByte(msg[i]);
+      }
+      Serial.println(msg);
+    }
   }
 }
